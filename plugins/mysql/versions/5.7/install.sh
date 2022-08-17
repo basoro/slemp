f0edd1565d098b39623efd00b1f89781c8110a15
Basoro <dentix.id@gmail.com>
2022-08-18 02:56:07 +0800

    Add MySQL Plugin
    Tambah plugin MySQL dengan fitur replikasi database Master-Slave

---
+++ plugins/mysql/versions/5.7/install.sh
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+#!/bin/bash
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+#https://dev.mysql.com/downloads/mysql/5.7.html
+#https://dev.mysql.com/downloads/file/?id=489855
+
+curPath=`pwd`
+rootPath=$(dirname "$curPath")
+rootPath=$(dirname "$rootPath")
+serverPath=$(dirname "$rootPath")
+sysName=`uname`
+
+
+install_tmp=${rootPath}/tmp/slemp_install.pl
+mysqlDir=${serverPath}/source/mysql
+
+VERSION="5.7.37"
+
+
+Install_mysql()
+{
+	mkdir -p ${mysqlDir}
+	echo 'installing script file...' > $install_tmp
+
+	if id mysql &> /dev/null ;then
+	    echo "mysql UID is `id -u www`"
+	    echo "mysql Shell is `grep "^www:" /etc/passwd |cut -d':' -f7 `"
+	else
+	    groupadd mysql
+		useradd -g mysql mysql
+	fi
+
+	if [ "$sysName" != "Darwin" ];then
+		mkdir -p /var/log/mariadb
+		touch /var/log/mariadb/mariadb.log
+	fi
+
+	# ----- cpu start ------
+	if [ -z "${cpuCore}" ]; then
+    	cpuCore="1"
+	fi
+
+	if [ -f /proc/cpuinfo ];then
+		cpuCore=`cat /proc/cpuinfo | grep "processor" | wc -l`
+	fi
+
+	MEM_INFO=$(free -m|grep Mem|awk '{printf("%.f",($2)/1024)}')
+	if [ "${cpuCore}" != "1" ] && [ "${MEM_INFO}" != "0" ];then
+	    if [ "${cpuCore}" -gt "${MEM_INFO}" ];then
+	        cpuCore="${MEM_INFO}"
+	    fi
+	else
+	    cpuCore="1"
+	fi
+
+	if [ "$cpuCore" -gt "1" ];then
+		cpuCore=`echo "$cpuCore" | awk '{printf("%.f",($1)*0.8)}'`
+	fi
+	# ----- cpu end ------
+
+	cd $serverPath/panel/plugins/mysql/lib && /bin/bash rpcgen.sh
+
+	if [ ! -f ${mysqlDir}/mysql-boost-${VERSION}.tar.gz ];then
+		wget -O ${mysqlDir}/mysql-boost-${VERSION}.tar.gz --tries=3 https://cdn.mysql.com/Downloads/MySQL-5.7/mysql-boost-${VERSION}.tar.gz
+	fi
+
+	#检测文件是否损坏.
+	md5_mysql_ok=d0489fc3880248a58759c50bfb286dbb
+	if [ -f ${mysqlDir}/mysql-boost-${VERSION}.tar.gz ];then
+		md5_mysql=`md5sum ${mysqlDir}/mysql-boost-${VERSION}.tar.gz  | awk '{print $1}'`
+		if [ "${md5_mysql_ok}" == "${md5_mysql}" ]; then
+			echo "mysql5.7 file check ok"
+		else
+			# 重新下载
+			rm -rf ${mysqlDir}/mysql-${VERSION}
+			wget -O ${mysqlDir}/mysql-boost-${VERSION}.tar.gz --tries=3 https://cdn.mysql.com/Downloads/MySQL-5.7/mysql-boost-${VERSION}.tar.gz
+		fi
+	fi
+
+	if [ ! -d ${mysqlDir}/mysql-${VERSION} ];then
+		 cd ${mysqlDir} && tar -zxvf  ${mysqlDir}/mysql-boost-${VERSION}.tar.gz
+	fi
+
+	OPTIONS=''
+	##check openssl version
+	OPENSSL_VERSION=`openssl version|awk '{print $2}'|awk -F '.' '{print $1}'`
+	if [ "${OPENSSL_VERSION}" -ge "3" ];then
+		#openssl version to high
+		cd $serverPath/panel/plugins/php/lib && /bin/bash openssl.sh
+		export PKG_CONFIG_PATH=$serverPath/lib/openssl/lib/pkgconfig
+		OPTIONS="-DWITH_SSL=${serverPath}/lib/openssl"
+	fi
+
+	if [ ! -d $serverPath/mysql ];then
+		cd ${mysqlDir}/mysql-${VERSION} && cmake \
+		-DCMAKE_INSTALL_PREFIX=$serverPath/mysql \
+		-DMYSQL_USER=mysql \
+		-DMYSQL_TCP_PORT=3306 \
+		-DMYSQL_UNIX_ADDR=/var/tmp/mysql.sock \
+		-DWITH_MYISAM_STORAGE_ENGINE=1 \
+		-DWITH_INNOBASE_STORAGE_ENGINE=1 \
+		-DWITH_MEMORY_STORAGE_ENGINE=1 \
+		-DENABLED_LOCAL_INFILE=1 \
+		-DWITH_PARTITION_STORAGE_ENGINE=1 \
+		-DEXTRA_CHARSETS=all \
+		-DDEFAULT_CHARSET=utf8mb4 \
+		-DDEFAULT_COLLATION=utf8mb4_general_ci \
+		-DDOWNLOAD_BOOST=1 \
+		$OPTIONS \
+		-DCMAKE_C_COMPILER=/usr/bin/gcc \
+		-DCMAKE_CXX_COMPILER=/usr/bin/g++ \
+		-DWITH_BOOST=${mysqlDir}/mysql-${VERSION}/boost/
+		make -j${cpuCore} && make install && make clean
+
+		if [ -d $serverPath/mysql ];then
+			echo '5.7' > $serverPath/mysql/version.pl
+			echo 'The installation is complete' > $install_tmp
+		else
+			# rm -rf ${mysqlDir}/mysql-${VERSION}
+			echo 'Installation failed' > $install_tmp
+			echo 'install fail'>&2
+			exit 1
+		fi
+	fi
+}
+
+Uninstall_mysql()
+{
+	rm -rf $serverPath/mysql
+	echo 'Uninstall complete' > $install_tmp
+}
+
+action=$1
+if [ "${1}" == "install" ];then
+	Install_mysql
+else
+	Uninstall_mysql
+fi
