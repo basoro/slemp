e0cdee79b40e604cf90c52319098c6d0d30edd15
Basoro <dentix.id@gmail.com>
2022-08-17 14:13:21 +0800

    Add openresty
    Add openresty as webserver to replace nginx

---
+++ plugins/openresty/index.py
@@ -0,0 +1,329 @@
+# coding:utf-8
+
+import sys
+import io
+import os
+import time
+import subprocess
+
+sys.path.append(os.getcwd() + "/class/core")
+import slemp
+
+
+app_debug = False
+
+if slemp.isAppleSystem():
+    app_debug = True
+
+
+def getPluginName():
+    return 'openresty'
+
+
+def getPluginDir():
+    return slemp.getPluginDir() + '/' + getPluginName()
+
+
+def getServerDir():
+    return slemp.getServerDir() + '/' + getPluginName()
+
+
+def getInitDFile():
+    if app_debug:
+        return '/tmp/' + getPluginName()
+    return '/etc/init.d/' + getPluginName()
+
+
+def getArgs():
+    args = sys.argv[2:]
+    tmp = {}
+    args_len = len(args)
+
+    if args_len == 1:
+        t = args[0].strip('{').strip('}')
+        t = t.split(':')
+        tmp[t[0]] = t[1]
+    elif args_len > 1:
+        for i in range(len(args)):
+            t = args[i].split(':')
+            tmp[t[0]] = t[1]
+
+    return tmp
+
+
+def clearTemp():
+    path_bin = getServerDir() + "/nginx"
+    slemp.execShell('rm -rf ' + path_bin + '/client_body_temp')
+    slemp.execShell('rm -rf ' + path_bin + '/fastcgi_temp')
+    slemp.execShell('rm -rf ' + path_bin + '/proxy_temp')
+    slemp.execShell('rm -rf ' + path_bin + '/scgi_temp')
+    slemp.execShell('rm -rf ' + path_bin + '/uwsgi_temp')
+
+
+def getConf():
+    path = getServerDir() + "/nginx/conf/nginx.conf"
+    return path
+
+
+def getConfTpl():
+    path = getPluginDir() + '/conf/nginx.conf'
+    return path
+
+
+def getOs():
+    data = {}
+    data['os'] = slemp.getOs()
+    ng_exe_bin = getServerDir() + "/nginx/sbin/nginx"
+    if checkAuthEq(ng_exe_bin, 'root'):
+        data['auth'] = True
+    else:
+        data['auth'] = False
+    return slemp.getJson(data)
+
+
+def getInitDTpl():
+    path = getPluginDir() + "/init.d/nginx.tpl"
+    return path
+
+
+def getFileOwner(filename):
+    import pwd
+    stat = os.lstat(filename)
+    uid = stat.st_uid
+    pw = pwd.getpwuid(uid)
+    return pw.pw_name
+
+
+def checkAuthEq(file, owner='root'):
+    fowner = getFileOwner(file)
+    if (fowner == owner):
+        return True
+    return False
+
+
+def confReplace():
+    service_path = os.path.dirname(os.getcwd())
+    content = slemp.readFile(getConfTpl())
+    content = content.replace('{$SERVER_PATH}', service_path)
+
+    user = 'www'
+    user_group = 'www'
+
+    if slemp.getOs() == 'darwin':
+        # macosx do
+        user = slemp.execShell(
+            "who | sed -n '2, 1p' |awk '{print $1}'")[0].strip()
+        # user = 'root'
+        user_group = 'staff'
+        content = content.replace('{$EVENT_MODEL}', 'kqueue')
+    else:
+        content = content.replace('{$EVENT_MODEL}', 'epoll')
+
+    content = content.replace('{$OS_USER}', user)
+    content = content.replace('{$OS_USER_GROUP}', user_group)
+
+    nconf = getServerDir() + '/nginx/conf/nginx.conf'
+    slemp.writeFile(nconf, content)
+
+    php_conf = slemp.getServerDir() + '/web_conf/php/conf'
+    if not os.path.exists(php_conf):
+        slemp.execShell('mkdir -p ' + php_conf)
+    static_conf = slemp.getServerDir() + '/web_conf/php/conf/enable-php-00.conf'
+    if not os.path.exists(static_conf):
+        slemp.writeFile(static_conf, 'set $PHP_ENV 0;')
+
+    # give nginx root permission
+    ng_exe_bin = getServerDir() + "/nginx/sbin/nginx"
+    if not checkAuthEq(ng_exe_bin, 'root'):
+        args = getArgs()
+        sudoPwd = args['pwd']
+        cmd_own = 'chown -R ' + 'root:' + user_group + ' ' + ng_exe_bin
+        os.system('echo %s|sudo -S %s' % (sudoPwd, cmd_own))
+        cmd_mod = 'chmod 755 ' + ng_exe_bin
+        os.system('echo %s|sudo -S %s' % (sudoPwd, cmd_mod))
+        cmd_s = 'chmod u+s ' + ng_exe_bin
+        os.system('echo %s|sudo -S %s' % (sudoPwd, cmd_s))
+
+
+def initDreplace():
+
+    file_tpl = getInitDTpl()
+    service_path = os.path.dirname(os.getcwd())
+
+    initD_path = getServerDir() + '/init.d'
+
+    # OpenResty is not installed
+    if not os.path.exists(getServerDir()):
+        print("ok")
+        exit(0)
+
+    # init.d
+    file_bin = initD_path + '/' + getPluginName()
+    if not os.path.exists(initD_path):
+        os.mkdir(initD_path)
+
+        # initd replace
+        content = slemp.readFile(file_tpl)
+        content = content.replace('{$SERVER_PATH}', service_path)
+        slemp.writeFile(file_bin, content)
+        slemp.execShell('chmod +x ' + file_bin)
+
+        # config replace
+        confReplace()
+
+    # systemd
+    # /usr/lib/systemd/system
+    systemDir = slemp.systemdCfgDir()
+    systemService = systemDir + '/openresty.service'
+    systemServiceTpl = getPluginDir() + '/init.d/openresty.service.tpl'
+    if os.path.exists(systemDir) and not os.path.exists(systemService):
+        se_content = slemp.readFile(systemServiceTpl)
+        se_content = se_content.replace('{$SERVER_PATH}', service_path)
+        slemp.writeFile(systemService, se_content)
+        slemp.execShell('systemctl daemon-reload')
+
+    return file_bin
+
+
+def status():
+    data = slemp.execShell(
+        "ps -ef|grep openresty |grep -v grep | grep -v python | awk '{print $2}'")
+    if data[0] == '':
+        return 'stop'
+    return 'start'
+
+
+def restyOp(method):
+    file = initDreplace()
+
+    check = getServerDir() + "/bin/openresty -t"
+    check_data = slemp.execShell(check)
+    if not check_data[1].find('test is successful') > -1:
+        return check_data[1]
+
+    if not slemp.isAppleSystem():
+        data = slemp.execShell('systemctl ' + method + ' openresty')
+        if data[1] == '':
+            return 'ok'
+        return data[1]
+
+    data = slemp.execShell(file + ' ' + method)
+    if data[1] == '':
+        return 'ok'
+    return data[1]
+
+
+def start():
+    return restyOp('start')
+
+
+def stop():
+    return restyOp('stop')
+
+
+def restart():
+    return restyOp('restart')
+
+
+def reload():
+    return restyOp('reload')
+
+
+def initdStatus():
+
+    if slemp.isAppleSystem():
+        return "Apple Computer does not support"
+
+    shell_cmd = 'systemctl status openresty | grep loaded | grep "enabled;"'
+    data = slemp.execShell(shell_cmd)
+    if data[0] == '':
+        return 'fail'
+    return 'ok'
+
+
+def initdInstall():
+    if slemp.isAppleSystem():
+        return "Apple Computer does not support"
+
+    slemp.execShell('systemctl enable openresty')
+    return 'ok'
+
+
+def initdUinstall():
+    if slemp.isAppleSystem():
+        return "Apple Computer does not support"
+
+    slemp.execShell('systemctl disable openresty')
+    return 'ok'
+
+
+def runInfo():
+    # 取Openresty负载状态
+    try:
+        url = 'http://' + slemp.getHostAddr() + '/nginx_status'
+        result = slemp.httpGet(url)
+        tmp = result.split()
+        data = {}
+        data['active'] = tmp[2]
+        data['accepts'] = tmp[9]
+        data['handled'] = tmp[7]
+        data['requests'] = tmp[8]
+        data['Reading'] = tmp[11]
+        data['Writing'] = tmp[13]
+        data['Waiting'] = tmp[15]
+        return slemp.getJson(data)
+    except Exception as e:
+        url = 'http://127.0.0.1/nginx_status'
+        result = slemp.httpGet(url)
+        tmp = result.split()
+        data = {}
+        data['active'] = tmp[2]
+        data['accepts'] = tmp[9]
+        data['handled'] = tmp[7]
+        data['requests'] = tmp[8]
+        data['Reading'] = tmp[11]
+        data['Writing'] = tmp[13]
+        data['Waiting'] = tmp[15]
+        return slemp.getJson(data)
+    except Exception as e:
+        return 'oprenresty not started'
+
+
+def errorLogPath():
+    return getServerDir() + '/nginx/logs/error.log'
+
+
+def installPreInspection():
+    return 'ok'
+
+
+if __name__ == "__main__":
+    func = sys.argv[1]
+    if func == 'status':
+        print(status())
+    elif func == 'start':
+        print(start())
+    elif func == 'stop':
+        print(stop())
+    elif func == 'restart':
+        print(restart())
+    elif func == 'reload':
+        print(reload())
+    elif func == 'initd_status':
+        print(initdStatus())
+    elif func == 'initd_install':
+        print(initdInstall())
+    elif func == 'initd_uninstall':
+        print(initdUinstall())
+    elif func == 'install_pre_inspection':
+        print(installPreInspection())
+    elif func == 'conf':
+        print(getConf())
+    elif func == 'get_os':
+        print(getOs())
+    elif func == 'run_info':
+        print(runInfo())
+    elif func == 'error_log':
+        print(errorLogPath())
+    else:
+        print('error')
---
+++ plugins/openresty/install.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+curPath=`pwd`
+rootPath=$(dirname "$curPath")
+rootPath=$(dirname "$rootPath")
+serverPath=$(dirname "$rootPath")
+
+
+action=$1
+type=$2
+
+VERSION=$2
+install_tmp=${rootPath}/tmp/slemp_install.pl
+openrestyDir=${serverPath}/source/openresty
+
+if id www &> /dev/null ;then
+    echo "www UID is `id -u www`"
+    echo "www Shell is `grep "^www:" /etc/passwd |cut -d':' -f7 `"
+else
+    groupadd www
+	  # useradd -g www -s /sbin/nologin www
+	  useradd -g www -s /bin/bash www
+fi
+
+# cd /home/slemp/server/panel/plugins/openresty && /bin/bash install.sh install 1.21.4.1
+Install_openresty()
+{
+	# ----- cpu start ------
+	if [ -z "${cpuCore}" ]; then
+    	cpuCore="1"
+	fi
+
+	if [ -f /proc/cpuinfo ];then
+		cpuCore=`cat /proc/cpuinfo | grep "processor" | wc -l`
+	fi
+
+	MEM_INFO=$(free -m|grep Mem|awk '{printf("%.f",($2)/1024)}')
+	if [ "${cpuCore}" != "1" ] && [ "${MEM_INFO}" != "0" ];then
+	    if [ "${cpuCore}" -gt "${MEM_INFO}" ];then
+	        cpuCore="${MEM_INFO}"
+	    fi
+	else
+	    cpuCore="1"
+	fi
+	# ----- cpu end ------
+
+	mkdir -p ${openrestyDir}
+	echo 'Installing script file...' > $install_tmp
+
+	if [ ! -f ${openrestyDir}/openresty-${VERSION}.tar.gz ];then
+		wget -O ${openrestyDir}/openresty-${VERSION}.tar.gz https://openresty.org/download/openresty-${VERSION}.tar.gz
+	fi
+
+
+	cd ${openrestyDir} && tar -zxvf openresty-${VERSION}.tar.gz
+
+	# --with-openssl=$serverPath/source/lib/openssl-1.0.2q
+	cd ${openrestyDir}/openresty-${VERSION} && ./configure \
+	--prefix=$serverPath/openresty \
+	--with-http_v2_module \
+	--with-http_ssl_module  \
+	--with-http_slice_module \
+	--with-http_stub_status_module
+
+	make -j${cpuCore} && make install && make clean
+
+	if [ -d $serverPath/openresty ];then
+		echo "${VERSION}" > $serverPath/openresty/version.pl
+
+		mkdir -p $serverPath/web_conf/php/conf
+		echo 'set $PHP_ENV 0;' > $serverPath/web_conf/php/conf/enable-php-00.conf
+
+		cd ${rootPath} && python3 ${rootPath}/plugins/openresty/index.py start
+		cd ${rootPath} && python3 ${rootPath}/plugins/openresty/index.py initd_install
+    fi
+	echo 'The installation is complete' > $install_tmp
+}
+
+Uninstall_openresty()
+{
+
+	if [ -f /usr/lib/systemd/system/openresty.service ] || [ -f /lib/systemd/system/openresty.service ];then
+		systemctl stop openresty
+		rm -rf /usr/systemd/system/openresty.service
+		rm -rf /lib/systemd/system/openresty.service
+		systemctl daemon-reload
+	fi
+
+	if [ -f $serverPath/openresty/init.d/openresty ];then
+		$serverPath/openresty/init.d/openresty stop
+	fi
+
+	rm -rf $serverPath/openresty
+	echo 'Uninstall complete' > $install_tmp
+}
+
+action=$1
+if [ "${1}" == 'install' ];then
+	Install_openresty
+else
+	Uninstall_openresty
+fi
