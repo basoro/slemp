533b1fbfc4aa405550153192e67a8d0c9f9fec88
Basoro <atilamedia.com@gmail.com>
2025-08-09 06:51:24 +0800

    Update

--- update.sh
+++ update.sh
@@ -5,8 +5,12 @@ echo "===> Updating SLEMP Panel..."
 echo "===> Download panel.zip..."
 wget https://github.com/basoro/slemp/archive/refs/heads/sabrina.zip  -O /tmp/panel.zip
 unzip /tmp/panel.zip -d /tmp
+rm -f /tmp/slemp-sabrina/data/config.json
 cp -R /tmp/slemp-sabrina/* /var/www/panel/
 rm -rf /tmp/slemp-sabrina
+
+echo "===> Restarting SLEMP Panel..."
+supervisorctl restart slemp
 
 supervisorctl start slemp
 
