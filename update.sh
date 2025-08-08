03068b529fde76ebb123997f6634844a6de86f2e
Basoro <atilamedia.com@gmail.com>
2025-08-09 07:15:06 +0800

    update

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ echo "===> Download panel.zip..."
 wget https://github.com/basoro/slemp/archive/refs/heads/sabrina.zip  -O /tmp/panel.zip
 unzip /tmp/panel.zip -d /tmp
 rm -f /tmp/slemp-sabrina/data/config.json
-cp -R /tmp/slemp-sabrina/* /var/www/panel/
+cp -aR /tmp/slemp-sabrina/* /var/www/panel/
 rm -rf /tmp/slemp-sabrina
 
 echo "===> Setup Python Virtual Environment..."
