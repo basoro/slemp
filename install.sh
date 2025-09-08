3dfd56d3f75458feb890efb7f56fc17c67bede7b
Basoro <dentix.id@gmail.com>
2025-09-08 09:17:06 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ apt-get clean && rm -rf /var/lib/apt/lists/*
 echo "===> Download panel.zip..."
 wget https://github.com/basoro/slemp/archive/refs/heads/sabrina.zip  -O ~/slemp.zip
 unzip ~/slemp.zip -d ~/slemp
-mv ~/slemp/slemp-sabrina /opt/slemp /panel
+mv ~/slemp/slemp-sabrina /opt/slemp/panel
 mkdir /opt/slemp/www
 mkdir /opt/slemp/mysql
 mkdir /opt/slemp/logs
