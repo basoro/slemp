5ba76d86234e906e774b6be44b9dc595dbcea571
Basoro <dentix.id@gmail.com>
2026-05-17 01:46:08 +0800

    refactor: remove interactive GitHub proxy selection logic from install and update scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -29,124 +29,6 @@ LOG_FILE=slemp-install.log
 {
 
 HTTP_PREFIX="https://"
-LOCAL_ADDR=common
-cn=$(curl -fsSL -m 10 -s http://ipinfo.io/json | grep "\"country\": \"CN\"")
-if [ ! -z "$cn" ] || [ "$?" == "0" ] ;then
-	LOCAL_ADDR=cn
-fi
-
-if [ "$LOCAL_ADDR" != "common" ];then
-	declare -A PROXY_URL
-	PROXY_URL["gh_proxy_com"]="https://gh-proxy.com/"
-    PROXY_URL["github_do"]="https://github.do/"
-    PROXY_URL["gh_llkk_cc"]="https://gh.llkk.cc/https://"
-    PROXY_URL["gh_felicity_ac_cn"]="https://gh.felicity.ac.cn/https://"
-    PROXY_URL["ghfast_top"]="https://ghfast.top/"
-    PROXY_URL["ghproxy_net"]="https://ghproxy.net/"
-    PROXY_URL["gh_927223_xyz"]="https://gh.927223.xyz/https://"
-    PROXY_URL["gh_proxy_net"]="https://gh-proxy.net/"
-    
-    PROXY_URL["source"]="https://"
-
-
-	SOURCE_LIST_KEY_SORT_TMP=$(echo ${!PROXY_URL[@]} | tr ' ' '\n' | sort -n)
-	SOURCE_LIST_KEY=(${SOURCE_LIST_KEY_SORT_TMP//'\n'/})
-	SOURCE_LIST_LEN=${#PROXY_URL[*]}
-fi
-
-
-function AutoSizeStr(){
-	NAME_STR=$1
-	NAME_NUM=$2
-
-	NAME_STR_LEN=`echo "$NAME_STR" | wc -L`
-	NAME_NUM_LEN=`echo "$NAME_NUM" | wc -L`
-
-	fix_len=35
-	remaining_len=`expr $fix_len - $NAME_STR_LEN - $NAME_NUM_LEN`
-	FIX_SPACE=' '
-	for ((ass_i=1;ass_i<=$remaining_len;ass_i++))
-	do 
-		FIX_SPACE="$FIX_SPACE "
-	done
-	echo -e " ❖   ${1}${FIX_SPACE}${2})"
-}
-
-function ChooseProxyURL(){
-	clear
-    echo -e '+---------------------------------------------------+'
-    echo -e '|                                                   |'
-    echo -e '|   =============================================   |'
-    echo -e '|                                                   |'
-    echo -e '|  Selamat datang di Skrip Instalasi Sekali Klik Panel Linux  |'
-    echo -e '|                                                   |'
-    echo -e '|   =============================================   |'
-    echo -e '|                                                   |'
-    echo -e '+---------------------------------------------------+'
-    echo -e ''
-    echo -e '#####################################################'
-    echo -e ''
-    echo -e '            Alamat proxy domestik berikut tersedia untuk dipilih:                 '
-    echo -e ''
-    echo -e '#####################################################'
-    echo -e ''
-    cm_i=0
-    for V in ${SOURCE_LIST_KEY[@]}; do
-    num=`expr $cm_i + 1`
-	AutoSizeStr "${V}" "$num"
-	cm_i=`expr $cm_i + 1`
-	done
-    echo -e ''
-    echo -e '#####################################################'
-    echo -e ''
-    echo -e "        Waktu Sistem  ${BLUE}$(date "+%Y-%m-%d %H:%M:%S")${PLAIN}"
-    echo -e ''
-    echo -e '#####################################################'
-    CHOICE_A=$(echo -e "\n${BOLD}└─ Silakan pilih dan masukkan alamat proxy yang ingin Anda gunakan [ 1-${SOURCE_LIST_LEN} ]：${PLAIN}")
-
-    read -p "${CHOICE_A}" INPUT
-    # echo $INPUT
-    if [ "$INPUT" == "" ];then
-        INPUT=1
-        TMP_INPUT=`expr $INPUT - 1`
-        INPUT_KEY=${SOURCE_LIST_KEY[$TMP_INPUT]}
-        echo -e "\nMemilih [${BLUE}${INPUT_KEY}${PLAIN}] secara default untuk instalasi!"
-    fi
-
-    if [ "$INPUT" -lt "0" ];then
-		INPUT=1
-		TMP_INPUT=`expr $INPUT - 1`
-		INPUT_KEY=${SOURCE_LIST_KEY[$TMP_INPUT]}
-		echo -e "\nError di bawah batas! Memilih [${BLUE}${INPUT_KEY}${PLAIN}] untuk instalasi!"
-		sleep 2s
-	fi
-
-	if [ "$INPUT" -gt "${SOURCE_LIST_LEN}" ];then
-		INPUT=${SOURCE_LIST_LEN}
-		TMP_INPUT=`expr $INPUT - 1`
-		INPUT_KEY=${SOURCE_LIST_KEY[$TMP_INPUT]}
-		echo -e "\nError di atas batas! Memilih [${BLUE}${INPUT_KEY}${PLAIN}] untuk instalasi!"
-		sleep 2s
-	fi
-
-    INPUT=`expr $INPUT - 1`
-    INPUT_KEY=${SOURCE_LIST_KEY[$INPUT]}
-    HTTP_PREFIX=${PROXY_URL[$INPUT_KEY]}
-}
-
-if [ "$LOCAL_ADDR" != "common" ];then
-	ChooseProxyURL
-
-	if [ "$HTTP_PREFIX" != "https://" ];then
-		DOMAIN=`echo $HTTP_PREFIX | sed 's|https://||g'`
-		DOMAIN=`echo $DOMAIN | sed 's|/||g'`
-		ping -c 3 $DOMAIN > /dev/null 2>&1
-		if [ "$?" != "0" ];then
-			echo "Alamat proxy tidak valid:${DOMAIN}"
-			exit
-		fi
-	fi
-fi
 
 if [ -f /etc/motd ];then
     echo "welcome to panel panel" > /etc/motd
@@ -209,7 +91,6 @@ if [ "$EUID" -ne 0 ] && [ "$OSNAME" != "macos" ];then
  	exit
 fi
 
-echo "LOCAL:${LOCAL_ADDR}"
 echo "OSNAME:${OSNAME}"
 
 if [ $OSNAME != "macos" ];then
