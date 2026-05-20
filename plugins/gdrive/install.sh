251179763a3844d159b468910bd7d07a6f74b30a
Basoro <dentix.id@gmail.com>
2026-05-20 21:02:45 +0800

    refactor: replace ping with curl check and simplify dependency installation in gdrive plugin

--- plugins/gdrive/install.sh
+++ plugins/gdrive/install.sh
@@ -31,8 +31,7 @@ VERSION=$2
 
 Install_App()
 {
-    tmp_ping=`ping -c 1 google.com 2>&1`
-    echo $tmp_ping
+    curl -s --connect-timeout 5 https://www.google.com > /dev/null 2>&1
     if [ $? -eq 0 ];then
         GDDIR=$serverPath/gdrive
         mkdir -p $GDDIR
@@ -45,45 +44,16 @@ Install_App()
                 echo "python3 < 3.10"
                 cd ${GDDIR} && python3 -m venv .
             fi
-            cd ${GDDIR} && source ${GDDIR}/bin/activate
-        else
-            cd ${GDDIR} && source ${GDDIR}/bin/activate
         fi
 
-        tmp=`python3 -V 2>&1|awk '{print $2}'`
-        pVersion=${tmp:0:3}
+        # Make pip installation robust using the virtualenv's direct pip binary
+        ${GDDIR}/bin/pip install --upgrade pip -i https://pypi.org/simple
+        ${GDDIR}/bin/pip install -I pyOpenSSL -i https://pypi.org/simple
+        ${GDDIR}/bin/pip install -I google-api-python-client==2.39.0 google-auth-httplib2==0.1.0 google-auth-oauthlib==0.5.0 -i https://pypi.org/simple
+        ${GDDIR}/bin/pip install -I httplib2==0.18.1 -i https://pypi.org/simple
 
-        which pip
-        if [ "$?" -eq "0" ];then
-            tmp=$(pip list|grep google-api-python-client|awk '{print $2}')
-            if [ "$tmp" != '2.39.0' ];then
-                pip install --upgrade google-api-python-client
-                # pip uninstall google-api-python-client -y
-                pip install -I google-api-python-client==2.39.0 -i https://pypi.Python.org/simple
-            fi
-            tmp=$(pip list|grep google-auth-httplib2|awk '{print $2}')
-            if [ "$tmp" != '0.1.0' ];then
-                pip uninstall google-auth-httplib2 -y
-                pip install -I google-auth-httplib2==0.1.0 -i https://pypi.Python.org/simple
-            fi
-            tmp=$(pip list|grep google-auth-oauthlib|awk '{print $2}')
-            if [ "$tmp" != '0.5.0' ];then
-                # pip uninstall google-auth-oauthlib -y
-                pip install -I google-auth-oauthlib==0.5.0 -i https://pypi.Python.org/simple
-            fi
-            tmp=$(pip list|grep -E '^httplib2'|awk '{print $2}')
-            if [ "$tmp" != '0.18.1' ];then
-                # pip uninstall httplib2 -y
-                pip install -I httplib2==0.18.1 -i https://pypi.Python.org/simple
-            fi
-        else
-            pip install -I pyOpenSSL
-            pip install -I google-api-python-client==2.39.0 google-auth-httplib2==0.1.0 google-auth-oauthlib==0.5.0 -i https://pypi.Python.org/simple
-            pip install -I httplib2==0.18.1 -i https://pypi.Python.org/simple
-        fi
         echo 'Sedang menginstal file skrip...'
 
-        
         echo "${VERSION}" > $serverPath/gdrive/version.pl
         echo 'Instalasi selesai'
     else
